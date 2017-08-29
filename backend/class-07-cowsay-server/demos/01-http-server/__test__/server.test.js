9a97e1388bf16dd59fc6e1afbaa730700f973ab4
Scott Schmidt <sjschmidt44@gmail.com>
2017-08-29 12:30:50 -0700

    update demo code for day 07

--- backend/class-07-cowsay-server/demos/01-http-server/__test__/server.test.js
+++ backend/class-07-cowsay-server/demos/01-http-server/__test__/server.test.js
@@ -11,9 +11,8 @@ describe('Testing the server file', function () {
   describe('POST method, /echo endpoint', () => {
     test('should return a status code of 200', done => {
       superagent.post('localhost:3000/echo')
-        .send({ 'value': 'hello-world' })
+        .send({'value': 'scott-is-awesome'})
         .set('Content-Type', 'application/json')
-        // .type('application/json')  <- show another way to set the content type
         .end((err, res) => {
           expect(err).toBeNull()
           expect(res.status).toBe(200)
@@ -23,35 +22,22 @@ describe('Testing the server file', function () {
 
     test('should respond with user input', done => {
       superagent.post('localhost:3000/echo')
-        .send({ 'value': 'hello-world' })
+        .send({'value': 'scott'})
         .set('Content-Type', 'application/json')
         .end((err, res) => {
           expect(err).toBeNull()
-          // expect(res.status).toBe(200)
-          expect(res.body.value).toBe('hello-world')
+          expect(res.body.value).toEqual('scott')
           done()
         })
     })
 
-    test('sent without a value', done => {
-      // leave this one in here as an additional point to discuss where we might improve our server.js validation
-      superagent.post('localhost:3000/echo')
-        .send({})
-        .set('Content-Type', 'application/json')
-        .end((err, res) => {
-          expect(err).toBeNull()
-          expect(res.status).toBe(200)
-          done()
-        })
-    })
 
     test('undefined endpoint', done => {
       superagent.post('localhost:3000/')
-        .send({ 'value': 'hello-world' })
+        .send({'value': 'scott'})
         .set('Content-Type', 'application/json')
         .end((err, res) => {
           expect(err).not.toBeNull()
-          expect(err).toBeInstanceOf(Error)
           expect(res.status).toBe(404)
           done()
         })
@@ -71,11 +57,9 @@ describe('Testing the server file', function () {
 
     test('should respond with the current date', done => {
       superagent.get('localhost:3000/time')
-        .set('Content-Type', 'application/json')
+        .type('application/json')
         .end((err, res) => {
           expect(err).toBeNull()
-          console.log('res', res.body)
-          // expect(res.body.date).toBeInstanceOf(Date)
           expect(res.body).toHaveProperty('now')
           expect(res.body).toHaveProperty('date')
           done()
@@ -83,7 +67,7 @@ describe('Testing the server file', function () {
     })
 
     test('undefined endpoint', done => {
-      superagent.get('localhost:3000/monkey')
+      superagent.get('localhost:3000/')
         .set('Content-Type', 'application/json')
         .end((err, res) => {
           expect(err).not.toBeNull()
